25204b9c9e984c18049cda8a4d33243885e68d1e
YS739 <suk921@gmail.com>
2022-12-08 14:32:30 +0900

    add input alert error should use value

--- src/App.js
+++ src/App.js
@@ -3,22 +3,22 @@ import Header from "./components/header/Header";
 import AddToDo from "./components/addToDo/AddToDo";
 
 // workingList component - destructuring
-function WorkingList({ title, body, isDone }) {
+function WorkingList({ title, content, isDone }) {
   return (
     <div className="list-box">
       <h2>{title}</h2>
-      <h4>{body}</h4>
+      <h4>{content}</h4>
       <button>삭제하기</button>
       <button>{isDone ? "취소" : "완료"}</button>
     </div>
   );
 }
 
-function DoneList({ title, body, isDone }) {
+function DoneList({ title, content, isDone }) {
   return (
     <div className="list-box">
       <h2>{title}</h2>
-      <h4>{body}</h4>
+      <h4>{content}</h4>
       <button>삭제하기</button>
       <button>{isDone ? "취소" : "완료"}</button>
     </div>
@@ -27,26 +27,33 @@ function DoneList({ title, body, isDone }) {
 
 const App = () => {
   const [toDoS, setToDoS] = useState([
-    { id: 1, title: "리액트", body: "리액트 과제 다시 하기", isDone: false },
-    { id: 2, title: "독서", body: "IT책 10장 읽기", isDone: true },
+    { id: 1, title: "리액트", content: "리액트 과제 다시 하기", isDone: false },
+    { id: 2, title: "독서", content: "IT책 10장 읽기", isDone: true },
   ]);
 
   const [title, setTitle] = useState("");
-  const [body, setBody] = useState("");
+  const [content, setContent] = useState("");
 
   // 추가하기 버튼을 누르면 새로운 ToDo가 추가
-  const onAddToDoHandler = () => {
-    const newToDoS = {
-      id: toDoS.length + 1,
-      title: title,
-      body: body,
-      isDone: false,
-    };
-    setToDoS([...toDoS, newToDoS]);
-    // input 초기화
-    setTitle("");
-    setBody("");
+  const onAddToDoHandler = (setTitle, setContent) => {
+    if (setTitle && setContent) {
+      const newToDoS = {
+        id: toDoS.length + 1,
+        title: title,
+        content: content,
+        isDone: false,
+      };
+      setToDoS([...toDoS, newToDoS]);
+      // input 초기화
+      setTitle("");
+      setContent("");
+    } else {
+      alert("제목과 내용을 모두 입력해주세요.");
+    }
   };
+
+  // 삭제하기 버튼
+  const deleteToDo = (id) => {};
 
   return (
     <div>
@@ -54,8 +61,8 @@ const App = () => {
       <AddToDo
         title={title}
         setTitle={setTitle}
-        body={body}
-        setBody={setBody}
+        content={content}
+        setContent={setContent}
         onAddToDoHandler={onAddToDoHandler}
       ></AddToDo>
 
@@ -67,7 +74,7 @@ const App = () => {
               <WorkingList
                 toDo={toDo}
                 title={toDo.title}
-                body={toDo.body}
+                content={toDo.content}
                 isDone={toDo.isDone}
                 key={toDo.id}
               ></WorkingList>
@@ -82,7 +89,7 @@ const App = () => {
               <DoneList
                 toDo={toDo}
                 title={toDo.title}
-                body={toDo.body}
+                content={toDo.content}
                 isDone={toDo.isDone}
                 key={toDo.id}
               ></DoneList>
--- src/components/addToDo/AddToDo.jsx
+++ src/components/addToDo/AddToDo.jsx
@@ -1,20 +1,28 @@
 import "./style.css";
 
-const AddToDo = ({ title, setTitle, body, setBody, onAddToDoHandler }) => {
+const AddToDo = ({ setTitle, setContent, onAddToDoHandler }) => {
+  const onChangeHandler = (e) => {
+    if (e.target.id === "title") {
+      setTitle(e.target.value);
+    } else if (e.target.id === "content") {
+      setContent(e.target.value);
+    }
+  };
+
   return (
     <div className="add-todo-box">
       <div className="input-box">
         <h3>제목</h3>
         <input
           placeholder="제목을 입력해주세요"
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
+          id="title"
+          onChange={onChangeHandler}
         />
         <h3>내용</h3>
         <input
           placeholder="내용을 입력해주세요"
-          value={body}
-          onChange={(e) => setBody(e.target.value)}
+          id="content"
+          onChange={onChangeHandler}
         />
       </div>
       <button onClick={onAddToDoHandler}>추가하기</button>
